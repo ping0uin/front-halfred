228a87cb5810537c6ecf4f701550364f76173d0e
10097800 <marc.lavieville@leroymerlin.fr>
2018-04-14 16:09:07 +0200

    feat: add landing with modification of vuetify.min.css in nodes_module

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,13 +1,17 @@
 <template>
     <v-toolbar>
       <v-toolbar-side-icon></v-toolbar-side-icon>
-      <v-toolbar-title>Title</v-toolbar-title>
-      <v-spacer></v-spacer>
+      <v-toolbar-title id="mainTitle">Halfred</v-toolbar-title>
       <v-toolbar-items class="hidden-sm-and-down">
         <v-btn flat>Link One</v-btn>
         <v-btn flat>Link Two</v-btn>
         <v-btn flat>Link Three</v-btn>
+        <v-btn flat>Link Four</v-btn>
+        <v-btn flat>Link Five</v-btn>
+        <v-btn flat>Link Six</v-btn>
       </v-toolbar-items>
+      <v-spacer></v-spacer>
+      <v-btn flat>Link Seven</v-btn>
     </v-toolbar>
 </template>
 
@@ -18,5 +22,7 @@ export default {
 </script>
 
 <style scoped>
-
+  .toolbar__title {
+    margin-right: 50px;
+  }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 
-import HelloWorld from '@/components/Landing/HelloWorld';
+import Landing from '@/components/Landing/Landing';
 
 Vue.use(Router);
 
@@ -10,7 +10,7 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: HelloWorld,
+      component: Landing,
     },
   ],
 });
